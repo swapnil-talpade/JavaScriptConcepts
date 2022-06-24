4369437e777f033e1ade86b8031373b6154e896e
swapnil-talpade <swapniltalpade41@gmail.com>
2022-06-24 10:41:28 +0530

    class methods

--- ObjectOriented/classes/index.js
+++ ObjectOriented/classes/index.js
@@ -1,16 +1,23 @@
 class UserInfo {
-  name;
-  age;
-  email;
   constructor(name, age, email) {
     this.name = name;
     this.age = age;
     this.email = email;
+  }
+  login() {
+    console.log(this.name, " is logged in");
+  }
+  logout() {
+    console.log(this.name, " is logged out");
   }
 }
 
 const userOne = new UserInfo("swapnil", 22, "swapnil@gmail.com");
 const userTwo = new UserInfo("harshal", 20, "harshal@gmail.com");
 
-console.log(userOne);
-console.log(userTwo);
+userOne.login();
+console.table(userOne);
+userOne.logout();
+userTwo.login();
+console.table(userTwo);
+userTwo.logout();
