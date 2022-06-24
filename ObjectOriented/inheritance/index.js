b1cba67e05e34c8d119b4ee0f41ceb57c5296c5e
swapnil-talpade <swapniltalpade41@gmail.com>
2022-06-24 16:01:09 +0530

    inheritance

---
+++ ObjectOriented/inheritance/index.js
@@ -0,0 +1,39 @@
+class UserInfo {
+  constructor(name, age, email) {
+    this.name = name;
+    this.age = age;
+    this.email = email;
+    this.score = 0;
+  }
+  login() {
+    console.log(this.name, " is logged in");
+    return this;
+  }
+  logout() {
+    console.log(this.name, " is logged out");
+    return this;
+  }
+  updateScore() {
+    this.score++;
+    console.log(this.name, " has a score of ", this.score);
+    return this;
+  }
+}
+
+class Admin extends UserInfo {
+  deleteUser(user) {
+    users = users.filter((u) => u.name !== user.name);
+  }
+}
+const userOne = new UserInfo("swapnil", 22, "swapnil@gmail.com");
+const userTwo = new UserInfo("harshal", 20, "harshal@gmail.com");
+const admin = new Admin("rahul", 24, "rahul@gmail.com");
+
+// for method chaining to work return this after every method
+userOne.login().updateScore().updateScore().updateScore().logout();
+userTwo.login().updateScore().updateScore().logout();
+
+let users = [userOne, userTwo, admin];
+console.log(users);
+admin.deleteUser(userTwo);
+console.log(users);
